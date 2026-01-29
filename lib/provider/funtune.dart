99f975fc7dc10b4e10b582aa1626a840dbdf70f9
yahyeabdinasirfarah <yahyeabdinasirfarah@gmail.com>
2026-01-29 22:27:53 +0300

    applying some changes

---
+++ lib/provider/funtune.dart
@@ -0,0 +1,12 @@
+// void main() {
+//   // List<String> rondomeFortune = ["mm", " kjj", " kjj", "hhhh", "mmh", "ppkkk"];
+//   // print(rondomeFortune.first);
+
+//   // final List<String> randomItemn = rondomeFortune.toList()..shuffle();
+//   // print(randomItemn);
+
+//   // //   for (var i = 0; i < 10; i++) {
+//   // //     print('hello ${i + 1}');
+//   //   }
+//   print("hello world ");
+// }
